d56d6a7c279f5c4978d0f7510ee3853acc105a3b
Scott Miller <miller.scott.thomas@gmail.com>
2022-07-23 15:53:19 -0400

    BrainBit in native Bluetooth for macOS. NOT for iOS.

--- BCILab/BrainFlow/Package.swift
+++ BCILab/BrainFlow/Package.swift
@@ -9,7 +9,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "BrainFlow",
-                 targets: ["BrainFlow", "BoardController", "DataHandler", "MLModule", "BrainBitLib"])
+                 targets: ["BrainFlow",
+                           "BoardController", "DataHandler", "MLModule", "BrainBitLib"])
     ],
     dependencies: [
         .package(name: "swift-numerics",
